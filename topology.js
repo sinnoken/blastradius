86c9324f6722671b20bc7e416c4599c87bbe7110
hsieh c. yicheng <sinnoken@live.com>
2026-05-27 14:59:30 +0800

    Update topology.js

--- topology.js
+++ topology.js
@@ -1,4 +1,4 @@
-// Exported from Topolograph POC at 2026-05-27T01:36:33.969Z
+// Exported from BlastRadius POC at 2026-05-27T06:59:05.210Z
 const topology = {
   nodes: [
     { id: "TPE", label: "TPE\nTW", type: "router", area: "0", stubs: ["1.1.1.1/32","10.1.0.0/24","100.64.0.0/24"], isASBR: true, isABR: false },
@@ -31,8 +31,7 @@ const topology = {
     { id: "e_FRA_PN", source: "FRA", target: "PN_EU", cost: 5, type: "transit" },
     { id: "e_AMS_PN", source: "AMS", target: "PN_EU", cost: 5, type: "transit" },
     { id: "e_LHR_FRA", source: "LHR", target: "FRA", cost: 10, type: "p2p" },
-    // Leased wave: SIN ↔ FRA(by Tata Communications, 10G long-term)
-    { id: "e_SIN_FRA_lease", source:"SIN", target:"FRA", cost: 80, type: "p2p" }
+    { id: "e_SIN_FRA_lease", source: "SIN", target: "FRA", cost: 80, type: "p2p" }
   ],
   externals: [
     { advertising_router: "TPE", subnet: "0.0.0.0/0", metric: 1, metric_type: "E2" }
@@ -42,12 +41,12 @@ const topology = {
     TYO: { x: 380, y: 130 },
     TPE: { x: 126, y: 111 },
     HKG: { x: 129, y: 285 },
-    SIN: { x: 275, y: 428 },
-    SYD: { x: 470, y: 324 },
+    SIN: { x: 297, y: 412 },
+    SYD: { x: 453, y: 279 },
     LAX: { x: 572, y: 134 },
-    LHR: { x: 760, y: 210 },
-    AMS: { x: 758, y: 77 },
-    FRA: { x: 961, y: 214 },
-    PN_EU: { x: 956, y: 68 }
+    LHR: { x: 753, y: 201 },
+    AMS: { x: 754, y: 42 },
+    FRA: { x: 754, y: 412 },
+    PN_EU: { x: 887, y: 194 }
   },
 };
